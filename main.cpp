c39e4bfc0ce3ef20d92648dc58f7bbacc9604c2b
Anugrah-T-Sebastian <anugrahsebastian98@gmail.com>
2024-04-04 12:57:09 +0100

    Wifi module integrated

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "SensorReader.h"
+#include "WifiManager.h"
 
 int main() {
     SensorReader sensor;
-    sensor.startReadingThread();
+    WifiManager wifiManager;
+
+    // Start SensorReader in a separate thread
+    std::thread sensorThread([&]() {
+        sensor.startReadingThread();
+    });
+
+    // Start WifiManager in a separate thread
+   std::thread wifiThread([&]() {
+        while (true) {
+            wifiManager.startNetworkProcess();
+            std::this_thread::sleep_for(std::chrono::seconds(20)); // Sleep for 20 seconds
+        }
+    });
+
+    // Join the threads
+
     std::cout << "Press Enter to quit" << std::endl;
     std::cin.get();
     sensor.stopReadingThread();
+    wifiThread.join();
+
     return 0;
 }
