4a8cb2289dd2ead9b5be59b9e15ad5a10b50bbe4
Anugrah-T-Sebastian <anugrahsebastian98@gmail.com>
2024-04-10 23:23:08 +0100

    Central server code modified to accept multiple requests

--- server.cpp
+++ server.cpp
@@ -8,6 +8,7 @@
 #include <cstring>
 
 int main() {
+    std::cout << "Starting server" << std::endl;
     //Create a socket
     int listening  = socket(AF_INET, SOCK_STREAM, 0);
     if(listening == -1){
@@ -18,7 +19,7 @@ int main() {
     // Bind the socket to a IP/Port
     sockaddr_in hint;
     hint.sin_family = AF_INET;
-    hint.sin_port = htons(1500);    // Host to network short i.e. conversion to little endian to big endian if neccessary
+    hint.sin_port = htons(1352);    // Host to network short i.e. conversion to little endian to big endian if neccessary
     inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 
     if(bind(listening, (sockaddr*)&hint, sizeof(hint)) == -1) {
@@ -32,58 +33,53 @@ int main() {
         return -3;
     }
 
-    // Accept a call
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    char host[NI_MAXHOST];
-    char svc[NI_MAXSERV];
+    while (true) {
+        // Accept a call
+        sockaddr_in client;
+        socklen_t clientSize = sizeof(client);
+        char host[NI_MAXHOST];
+        char svc[NI_MAXSERV];
 
-    int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+        int clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
 
-    if(clientSocket == -1) {
-        std::cerr << "Problem with client connecting!";
-        return -4;
-    }
+        if(clientSocket == -1) {
+            std::cerr << "Problem with client connecting!";
+            continue;
+        }
 
-    // Close the listening socket
-    close(listening);
+        memset(host, 0, NI_MAXHOST);
+        memset(svc, 0, NI_MAXSERV);
 
-    memset(host, 0, NI_MAXHOST);
-    memset(svc, 0, NI_MAXSERV);
+        int result = getnameinfo((
+                            sockaddr*)&client, 
+                            sizeof(client),
+                            host,
+                            NI_MAXHOST,
+                            svc,
+                            NI_MAXSERV,
+                            0
+                        );
 
-    int result = getnameinfo((
-                        sockaddr*)&client, 
-                        sizeof(client),
-                        host,
-                        NI_MAXHOST,
-                        svc,
-                        NI_MAXSERV,
-                        0
-                    );
+        if (result) {
+            std::cout << host << " connected on " << svc << std::endl;
+        }
+        else {
+            inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+            std::cout << host << "Connected on " << ntohs(client.sin_port) << std::endl;
+        }
 
-    if (result) {
-        std::cout << host << " connected on " << svc << std::endl;
-    }
-    else {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        std::cout << host << "Connected on " << ntohs(client.sin_port) << std::endl;
-    }
+        // While receiving - display message, echo message
+        char buf[4096];
 
-    // While receiving - display message, echo message
-    char buf[4096];
-    while (true)
-    {
         // Clear the buffer
         memset(buf, 0, 4096);
         int bytesRecv = recv(clientSocket, buf, 4096, 0);
         if(bytesRecv == -1) {
             std::cerr << "There was a connection issue" << std::endl;
-            break;
         }
 
         if(bytesRecv == 0) {
             std::cerr << "The client was disconnected" << std::endl;
-            break;
         }
 
         // Display message
@@ -93,9 +89,13 @@ int main() {
 
         // Resend message
         send(clientSocket, response.c_str(), response.size() + 1, 0);
+
+        // Close the client socket
+        close(clientSocket);
     }
 
-    close(clientSocket);
-    
+    // Close the listening socket
+    close(listening);
+
     return 0;
 }
